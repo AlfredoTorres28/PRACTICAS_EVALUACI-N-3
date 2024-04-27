76ba20a3a98b5355088579b8356e51fe446417b0
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2024-04-27 15:45:16 -0600

    Add files via upload

---
+++ EVA3_6_ARREGLOS_MULTI/src/eva3_6_arreglos_multi/EVA3_6_ARREGLOS_MULTI.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_6_arreglos_multi;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA3_6_ARREGLOS_MULTI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int matriz[][] = new int[3][2];
+        matriz[0][0] = 100;
+        matriz[0][1] = 100;
+        matriz[1][0] = 100;
+        matriz
+        
+    
+}
