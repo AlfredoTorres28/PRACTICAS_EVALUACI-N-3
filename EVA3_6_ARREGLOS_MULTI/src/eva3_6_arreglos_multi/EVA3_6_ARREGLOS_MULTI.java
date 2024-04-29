d1375630c8290924f4d1c904ee385bba48a4c9ad
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2024-04-29 16:35:37 -0600

    Add files via upload

--- EVA3_6_ARREGLOS_MULTI/src/eva3_6_arreglos_multi/EVA3_6_ARREGLOS_MULTI.java
+++ EVA3_6_ARREGLOS_MULTI/src/eva3_6_arreglos_multi/EVA3_6_ARREGLOS_MULTI.java
@@ -17,9 +17,19 @@ public class EVA3_6_ARREGLOS_MULTI {
         // TODO code application logic here
         int matriz[][] = new int[3][2];
         matriz[0][0] = 100;
-        matriz[0][1] = 100;
-        matriz[1][0] = 100;
-        matriz
+        matriz[1][0] = 200;
+        matriz[2][0] = 300;
+        matriz[0][1] = 400;
+        matriz[1][1] = 500;
+        matriz[2][1] = 600;
         
-    
+        System.out.println("matriz lenght -" + matriz.length);
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println("matriz(" + i + ")(" + j +") = " + matriz[i][j]);
+            }
+        }
+            
+        }
+         
 }
