170d103c3cd5fa533311a1262719dda080588dc6
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2024-04-29 22:45:04 -0600

    Add files via upload

---
+++ EVA3_ 8_ CUBO/src/eva3_/pkg8_/cubo/EVA3_8_CUBO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_.pkg8_.cubo;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA3_8_CUBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cubo[][][] = new int[10][5][3];
+        for (i)
+    }
+    
+}
