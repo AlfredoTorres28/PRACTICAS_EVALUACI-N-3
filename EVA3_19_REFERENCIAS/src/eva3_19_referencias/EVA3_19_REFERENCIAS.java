e56a8b2a38b45c634c708c21cb24497f311d9540
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2024-06-07 17:10:24 -0600

    Add files via upload

---
+++ EVA3_19_REFERENCIAS/src/eva3_19_referencias/EVA3_19_REFERENCIAS.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_19_referencias;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA3_19_REFERENCIAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int[] arreglo = new int [5];
+        int x = 100;
+        System.out.println("Antes de llamar al metodo. x = " + x);
+        sumarUno(x);
+        System.out.println("Ddespues de lllamar al metodo. x = " + x);
+        System.out.println("-------------------------------------------");
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("[" + arreglo[i] + "]");
+        }
+        System.out.println("");
+        modificarArreglo(arreglo);
+       for (int i = 0; i < arreglo.length; i++) {
+           System.out.println("[" + arreglo[i] + "]");
+           
+       }
+    }
+    public static void sumarUno(int valor) {
+        System.out.println("Amntes de la operacion, valor: "+ valor);
+        valor = valor  + 1;
+         System.out.println("Despues de la operacion. valor: "+ valor);
+    
+}
+     public static void modificarArreglo (int [] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo [i] = (int) (Math.random() * 100);
+        }
+    }
+    
+}
