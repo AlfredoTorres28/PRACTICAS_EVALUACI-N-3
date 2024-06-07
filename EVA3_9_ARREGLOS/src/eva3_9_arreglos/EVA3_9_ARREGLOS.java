3cddab4f5a15e034179cdd8bc896b9f706bd6f04
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2024-06-07 17:08:04 -0600

    Add files via upload

---
+++ EVA3_9_ARREGLOS/src/eva3_9_arreglos/EVA3_9_ARREGLOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_9_arreglos;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA3_9_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String diasSemana[] = new String[7];
+        diasSemana[0] = {"Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
+        char vocales[] = {'a', 'e', 'i', 'o', 'u'};
+                System.out.println("Tamaño del arreglo diasSemana");
+                
+    }
+    
+}
