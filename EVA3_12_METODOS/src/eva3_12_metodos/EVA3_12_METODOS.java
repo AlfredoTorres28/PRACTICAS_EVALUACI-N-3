33c65b2dc5a2541ed1a2bca7860821b73dd65475
AlfredoTorres28 <159494258+AlfredoTorres28@users.noreply.github.com>
2024-06-07 17:09:01 -0600

    Add files via upload

---
+++ EVA3_12_METODOS/src/eva3_12_metodos/EVA3_12_METODOS.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_12_metodos;
+
+/**
+ *
+ * @author jealt
+ */
+public class EVA3_12_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Invocar al metodo ---> llamada ---> call
+        mostrarMensaje("Hola Mundo!!");
+    }
+    
+    //public static --> para la materia POO
+    //void --> (tipo de dato) valor que regresa el metodo
+    //void --> no regresa nada
+    //nombre del metodo --> empezar en minusculas, debe ser un verbo
+    //() <--- parametros del metodo (argumentos) ---> valores que necesite 
+    //para funcionar
+    
+    public static void mostrarMensaje(String mensaje){
+        ///cuerpo del metodo aqui va nuestro codigo
+        System.out.println(mensaje);
+    }
+    
+    public static void mostrarMuchosMnesajes(String mensaje, int veces) {
+        for (int i = 0)
+    }
+    
+}
